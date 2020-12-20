16cb88a1321627e695944efe4bf7064088148da5
Sukanya3096 <isukanyadutta@gmail.com>
2020-12-21 01:37:38 +0530

    added service layer

--- src/main/java/techicalblog/controller/HomeController.java
+++ src/main/java/techicalblog/controller/HomeController.java
@@ -1,9 +1,11 @@
 package techicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import techicalblog.model.PostModel;
+import techicalblog.service.PostService;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,21 +13,12 @@ import java.util.Date;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PostService PostService;
+
     @RequestMapping("/")
     public String getAllPosts(Model model) {
-        ArrayList<PostModel> posts = new ArrayList<>();
-        PostModel post1 = new PostModel();
-        post1.setTitle("Post Title 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
-
-        PostModel post2 = new PostModel();
-        post2.setTitle("Post Title 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
+        ArrayList<PostModel> posts = PostService.getAllPosts();
 
         model.addAttribute("posts", posts);
 
---
+++ src/main/java/techicalblog/service/PostService.java
@@ -0,0 +1,35 @@
+package techicalblog.service;
+
+import org.springframework.stereotype.Service;
+import techicalblog.model.PostModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Service
+public class PostService {
+    public ArrayList<PostModel> getAllPosts() {
+        ArrayList<PostModel> posts = new ArrayList<>();
+
+        PostModel post1 = new PostModel();
+        post1.setTitle("Post 1");
+        post1.setBody("Post Body 1");
+        post1.setDate(new Date());
+
+        PostModel post2 = new PostModel();
+        post2.setTitle("Post 2");
+        post2.setBody("Post Body 2");
+        post2.setDate(new Date());
+
+        PostModel post3 = new PostModel();
+        post3.setTitle("Post 3");
+        post3.setBody("Post Body 3");
+        post3.setDate(new Date());
+
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+
+        return posts;
+    }
+}
